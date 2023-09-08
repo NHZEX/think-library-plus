38aefd57b7ba2c2f0b8272cd5c53280c9884b3c6
auooru <auooru@outlook.com>
2023-09-09 01:13:23 +0800

    update: rector.php

--- rector.php
+++ rector.php
@@ -2,13 +2,22 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Foreach_\UnusedForeachValueToArrayKeysRector;
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/src',
+        __DIR__ . '/legacy',
+        __DIR__ . '/bin',
+    ]);
+
+    $rectorConfig->skip([
+        MixedTypeRector::class,
+        UnusedForeachValueToArrayKeysRector::class,
     ]);
 
     $rectorConfig->sets([
