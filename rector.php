085ba52b5982dc5ab323420c82d0b535678560b6
auooru <auooru@outlook.com>
2024-04-24 15:53:57 +0800

    update: 更新静态分析依赖

--- rector.php
+++ rector.php
@@ -5,30 +5,22 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Foreach_\UnusedForeachValueToArrayKeysRector;
 use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/legacy',
         __DIR__ . '/bin',
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         MixedTypeRector::class,
         UnusedForeachValueToArrayKeysRector::class,
         DisallowedEmptyRuleFixerRector::class,
+    ])
+    ->withPreparedSets(deadCode: true, codeQuality: false, codingStyle: false)
+    ->withImportNames(importShortClasses: false)
+    ->withPhpSets()
+    ->withPHPStanConfigs([
+        __DIR__ . '/phpstan.neon',
     ]);
-
-    $rectorConfig->sets([
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        LevelSetList::UP_TO_PHP_80,
-    ]);
-
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
-};
